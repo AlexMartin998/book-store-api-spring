f17bc918e45be0090ae4438a2a27da1b34f6a19c
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-09 07:08:36 -0500

    [ADD]: handleMethodArgumentNotValid for GlobalExceptionHandler (@Valid)

--- src/main/java/com/alex/security/common/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/alex/security/common/exceptions/GlobalExceptionHandler.java
@@ -1,68 +1,55 @@
 package com.alex.security.common.exceptions;
 
 import com.alex.security.common.dto.ErrorDetailsDto;
+import lombok.NonNull;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 // Handler de TODAS las exceptions de nuestra App. -- basta con esto, ya NOO necesita mas nada, ni ser importado ni notificado a Spring
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {  // extends para crear el handler del @Valid
 
-    // // Authentication
+    // // Authentication || UsernameNotFoundException
     @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ErrorDetailsDto> handlerBadCredentialsException(
-            BadCredentialsException exception,
-            WebRequest webRequest
-    ) {
-
-        ErrorDetailsDto errorDetails = ErrorDetailsDto.builder()
-                .timeStamp(new Date())
-                .message("There was a problem logging in. Check your email and password or create an account")
-                .details(webRequest.getDescription(false))
-                .build();
-
-        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<ErrorDetailsDto> handlerBadCredentialsException(BadCredentialsException exception, WebRequest webRequest) {
+        return createErrorResponse(
+                exception,
+                "There was a problem logging in. Check your email and password or create an account",
+                HttpStatus.UNAUTHORIZED,
+                webRequest
+        );
     }
 
     // // Authorization
     @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<ErrorDetailsDto> handlerBadCredentialsException(
-            AccessDeniedException exception,
-            WebRequest webRequest
-    ) {
-
-        ErrorDetailsDto errorDetails = ErrorDetailsDto.builder()
-                .timeStamp(new Date())
-                .message("User without required permissions!")
-                .details(webRequest.getDescription(false))
-                .build();
-
-        return new ResponseEntity<>(errorDetails, HttpStatus.FORBIDDEN);
+    public ResponseEntity<ErrorDetailsDto> handlerAccessDeniedException(AccessDeniedException exception, WebRequest webRequest) {
+        return createErrorResponse(exception, "User without required permissions", HttpStatus.FORBIDDEN, webRequest);
     }
 
     // // UsernameNotFoundException
     @ExceptionHandler(UsernameNotFoundException.class)
-    public ResponseEntity<ErrorDetailsDto> handlerUsernameNotFoundException(
-            UsernameNotFoundException exception,
-            WebRequest webRequest
-    ) {
-        ErrorDetailsDto errorDetails = ErrorDetailsDto.builder()
-                .timeStamp(new Date())
-                .message("There was a problem logging in. Check your email and password or create an account")
-                .details(webRequest.getDescription(false))
-                .build();
-
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorDetailsDto> handlerUsernameNotFoundException(UsernameNotFoundException exception, WebRequest webRequest) {
+        return createErrorResponse(
+                exception,
+                "There was a problem logging in. Check your email and password or create an account",
+                HttpStatus.NOT_FOUND,
+                webRequest);
     }
 
     // // Las Exceptions de Filter (Security|JWT) NO se atrapan aqui, las manejo en el JwtService & EntryPoint)
@@ -71,32 +58,58 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {  //
 
     // // User not found exception
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<ErrorDetailsDto> handlerUserNotFoundException(
-            UserNotFoundException exception,
-            WebRequest webRequest
-    ) {
-        ErrorDetailsDto errorDetails = ErrorDetailsDto.builder()
-                .timeStamp(new Date())
-                .message(exception.getMessage())
-                .details(webRequest.getDescription(false))
-                .build();
-
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorDetailsDto> handlerUserNotFoundException(UserNotFoundException exception, WebRequest webRequest) {
+        return createErrorResponse(exception, exception.getMessage(), HttpStatus.NOT_FOUND, webRequest);
     }
 
     // // Bad request exception
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<ErrorDetailsDto> handlerBadRequestException(
-            BadRequestException exception,
-            WebRequest webRequest
+    public ResponseEntity<ErrorDetailsDto> handlerBadRequestException(BadRequestException exception, WebRequest webRequest) {
+        return createErrorResponse(exception, exception.getMessage(), HttpStatus.BAD_REQUEST, webRequest);
+    }
+
+    // // Resource Not Found Exception
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetailsDto> handlerResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest) {
+        return createErrorResponse(exception, exception.getMessage(), HttpStatus.NOT_FOUND, webRequest);
+    }
+
+
+    // // Default Exception (for ALL others)
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetailsDto> handlerGlobalException(Exception exception, WebRequest webRequest) {
+        return createErrorResponse(exception, exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, webRequest);
+    }
+
+
+    // // Spring Validation (@Valid): handler de los errors del @Valid <- BindingResult
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            @NonNull HttpHeaders headers,
+            @NonNull HttpStatusCode status,
+            @NonNull WebRequest request
     ) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(err -> {
+            String fieldName = ((FieldError) err).getField();
+            String message = err.getDefaultMessage();
+
+            errors.put(fieldName, message);
+        });
+
+        return new ResponseEntity<>(errors, status);
+    }
+
+
+    private ResponseEntity<ErrorDetailsDto> createErrorResponse(Exception exception, String message, HttpStatus httpStatus, WebRequest webRequest) {
         ErrorDetailsDto errorDetails = ErrorDetailsDto.builder()
                 .timeStamp(new Date())
-                .message(exception.getMessage())
+                .message(message)
                 .details(webRequest.getDescription(false))
                 .build();
 
-        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorDetails, httpStatus);
     }
 
 }
