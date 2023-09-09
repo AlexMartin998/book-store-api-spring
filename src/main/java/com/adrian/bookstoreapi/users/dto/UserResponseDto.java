35d218f4ab6c9dbec8554f0a9c0ea399a2523437
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-09 08:16:06 -0500

    [ADD]: UserResponseDto

---
+++ src/main/java/com/adrian/bookstoreapi/users/dto/UserResponseDto.java
@@ -0,0 +1,16 @@
+package com.adrian.bookstoreapi.users.dto;
+
+import com.adrian.bookstoreapi.auth.dto.AuthResponseDto;
+import lombok.Data;
+
+
+@Data
+public class UserResponseDto {
+
+    private long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private AuthResponseDto.RoleDto role;
+
+}
