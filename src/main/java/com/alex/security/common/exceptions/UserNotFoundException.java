f922f771a0e0698e4909765b5e5fdb78fb7e479b
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-09 07:08:23 -0500

    [ADD]: UserNotFoundException (custom exception)

---
+++ src/main/java/com/alex/security/common/exceptions/UserNotFoundException.java
@@ -0,0 +1,18 @@
+package com.alex.security.common.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+
+    private String message;
+
+}
