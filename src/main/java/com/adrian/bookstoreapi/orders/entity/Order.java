8592dbdf19d9020c6d83dc1acb4d3f8c97dba593
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-09 15:42:53 -0500

    [ADD]: Order Entity

---
+++ src/main/java/com/adrian/bookstoreapi/orders/entity/Order.java
@@ -0,0 +1,46 @@
+package com.adrian.bookstoreapi.orders.entity;
+
+import com.adrian.bookstoreapi.common.constants.PaymentStatus;
+import com.adrian.bookstoreapi.users.entity.Usuario;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Data
+@Entity
+@Table(name = "_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonBackReference("customer_order_ref")
+    private Usuario customer;
+
+    @OneToMany(mappedBy = "order", fetch = FetchType.EAGER)
+    @JsonManagedReference("order_orderitem_ref")
+    private List<OrderItem> orderItems;
+
+
+}
