1253c71dffcff7a9eda72a844e624a4e9092fa9f
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-09 17:05:39 -0500

    [ADD]: BookHomeResponseDto

---
+++ src/main/java/com/adrian/bookstoreapi/home/controller/BookHomeResponseDto.java
@@ -0,0 +1,21 @@
+package com.adrian.bookstoreapi.home.controller;
+
+import com.adrian.bookstoreapi.categories.dto.SimpleCategoryDto;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+
+@Data
+public class BookHomeResponseDto {
+
+    private Long id;
+    private String title;
+    private BigDecimal price;
+    private String slug;
+    private String description;
+    private String coverPath;
+    private String filePath;
+    private SimpleCategoryDto category;
+
+}
