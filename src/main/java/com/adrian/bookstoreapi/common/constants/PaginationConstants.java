d21308948a949257f4316c4c858281f392e60554
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-09 07:09:01 -0500

    [ADD]: PaginationConstants

---
+++ src/main/java/com/adrian/bookstoreapi/common/constants/PaginationConstants.java
@@ -0,0 +1,12 @@
+package com.adrian.bookstoreapi.common.constants;
+
+
+public class PaginationConstants {
+
+    public static final String DEFAULT_PAGE = "0";
+    public static final String DEFAULT_SIZE = "10";
+    public static final String DEFAULT_SORT_BY = "id";
+    public static final String DEFAULT_SORT_DIR = "asc";
+
+}
+
