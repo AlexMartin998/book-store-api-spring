72a9ae57e1bbb3c50c16b708e87b3b6242511663
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-09 07:08:31 -0500

    [ADD]: Auth DTO's

---
+++ src/main/java/com/alex/security/auth/dto/LoginRequestDto.java
@@ -0,0 +1,24 @@
+package com.alex.security.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+
+@Data
+public class LoginRequestDto {
+
+    @NotEmpty
+    @Email
+    private String email;
+
+    @NotEmpty
+    @NotEmpty
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, one digit, and one special character."
+    )
+    private String password;
+
+}
