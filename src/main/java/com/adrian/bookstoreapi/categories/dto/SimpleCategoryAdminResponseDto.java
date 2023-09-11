315f4787855d22fb85c5d4010d450bcc791580bd
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-11 16:29:40 -0500

    [ADD]: SimpleCategoryAdminResponseDto

---
+++ src/main/java/com/adrian/bookstoreapi/categories/dto/SimpleCategoryAdminResponseDto.java
@@ -0,0 +1,11 @@
+package com.adrian.bookstoreapi.categories.dto;
+
+import lombok.Data;
+
+@Data
+public class SimpleCategoryAdminResponseDto {
+
+    private Long id;
+    private String name;
+
+}
